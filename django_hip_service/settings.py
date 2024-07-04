bea5a2dbea50115c0a0d25006b49b982db29ef2e
liaozhimingandy@qq.com <liaozhimingandy@qq.com>
2024-07-04 09:09:30 +0800

    perf(core): 重构
    1.完善部分代码

--- django_hip_service/settings.py
+++ django_hip_service/settings.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 # 应用版本号
-VERSION = (24, 6, 0, "alpha", 1)
+VERSION = (24, 7, 0, "alpha", 1)
 __version__ = get_version(VERSION)
 APP_NAME = "HIP"
 # id前缀
