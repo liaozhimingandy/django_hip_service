48b24e43dafc19813ba2055391bc54d454d57d28
liaozhimingandy@qq.com <liaozhimingandy@qq.com>
2025-06-25 17:13:37 +0800

    fix(core): 修复已知bug
    1.修复已知bug

--- scripts/build-base-image.sh
+++ scripts/build-base-image.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-RELEASE_DIR='./docker';
+# 获取脚本所在目录
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# 假设项目根是脚本所在目录的上一级
+PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
+
+# RELEASE_DIR 相对于项目根
+RELEASE_DIR="$PROJECT_ROOT/docker"
+
 REPO_NAME='liaozhiming/django_hip_service'
 
 for app_ver in $RELEASE_DIR/*; do
@@ -18,7 +25,16 @@ for app_ver in $RELEASE_DIR/*; do
         echo $app_ver
         echo "VERSION_PYTHON_TAG: $VERSION_PYTHON_TAG"
 
-        docker build --build-arg VERSION_PYTHON_TAG=$VERSION_PYTHON_TAG -f "$app_ver/Dockerfile.base" -t "$REPO_NAME:$VERSION_PYTHON_TAG-base" "$app_ver"
+         # Dockerfile 路径
+        DOCKERFILE_PATH="$app_ver/Dockerfile.base"
+        # 上下文设为项目根
+        CONTEXT_DIR="$PROJECT_ROOT"
+
+        echo "Building with Dockerfile: $DOCKERFILE_PATH"
+        echo "Build context: $CONTEXT_DIR"
+
+        docker build --build-arg VERSION_PYTHON_TAG=$VERSION_PYTHON_TAG -f "$app_ver/Dockerfile.base" \
+        -t "$REPO_NAME:$VERSION_PYTHON_TAG-base" "$CONTEXT_DIR"
     fi
 
 done
